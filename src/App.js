eba8b84249257bf3d9416a1841bd45afcc038c74
Bogdan Modolea <bogdanmodolea30@gmail.com>
2023-01-22 22:34:10 +0200

    Modified API

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
 
   const [testData, setTestData] = useState({});
 
-  const riotKey = 'RGAPI-19148a40-f777-418c-aad1-31d3d96547d8';
+  const riotKey = 'RGAPI-4ab7e052-9239-4c85-abc2-061a157beb0b';
   
   let fabian = 'N954To8SyMXoTd2JhRlMg200GyNNHOG0JbZ04Ef3URfLg2Y';
   let leo = 'NI6JBJ7tYn407b1fnaOFv7AFTDhMqtkE6V30lWBopj1Annk';
